2822a0bf598a537676fe862a83f829daacdecec1
Sulan <k.sulanowski2015@gmail.com>
2022-06-22 18:06:18 +0200

    Okienka powiadomień po usunięciu/dodaniu rekordu

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -10,6 +10,7 @@ use Illuminate\Contracts\View\View;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 
 class ProductController extends Controller
 {
@@ -108,6 +109,7 @@ class ProductController extends Controller
 
         try {
             $product->delete();
+            Session::flash('status', __('shop.product.status.delete.success'));
             return response()->json([
                 'status' => 'success'
             ]);
